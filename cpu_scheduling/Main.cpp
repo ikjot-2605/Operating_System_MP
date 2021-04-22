b561fb35496be3bb45b6f84614a10b185dad1e07
Himanshu8862 <81471989+Himanshu8862@users.noreply.github.com>
2021-04-22 21:19:08 +0530

    Update Main.cpp

--- cpu_scheduling/Main.cpp
+++ cpu_scheduling/Main.cpp
@@ -10,13 +10,14 @@
 using namespace std;
 
 int cpu_main() {
+    int choice;
     while (true) {
         cout << "Choose one of the following options..." << endl;
         cout << "0. Go back to main menu." << endl;
         cout << "1. First Come First Serve Demo" << endl;
-        cout << "2. Highest Respone Ratio Next Demo" << endl;
+        cout << "2. Highest Response Ratio Next Demo" << endl;
         cout << "3. Longest Job First Demo" << endl;
-        cout << "4. Longest Rmaining Time First Demo" << endl;
+        cout << "4. Longest Remaining Time First Demo" << endl;
         cout << "5. Round Robin Demo" << endl;
         cout << "6. Shortest Job First Demo" << endl;
         cout << "7. Shortest Remaining Time First Demo" << endl;
@@ -24,7 +25,7 @@ int cpu_main() {
         if (choice == 0) break;
         else {
             switch (choice) {
-                case 1: fcfs_main(); break;
+                case 1: cpu_fcfs_main(); break;
                 case 2: hrrn_main(); break;
                 case 3: ljf_main(); break;
                 case 4: lrtf_main(); break;
